e36cb9af8bd02c3cefc0d619fea165345fc93133
Larissa C <104375004+Lariczak@users.noreply.github.com>
2024-09-19 15:26:54 -0300

    sasasa

--- Gráficos/quantidadeUsuarios.js
+++ Gráficos/quantidadeUsuarios.js
@@ -4,8 +4,8 @@ async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = Object.keys(dados)
-    const quantidadeUsuarios = Object.values(dados)
+    const nomeDasRedes = ['Nordeste','Sudeste','Norte']
+    const quantidadeUsuarios = ['3171','1359','873']
     const data = [
         {
             x: nomeDasRedes,
@@ -31,7 +31,7 @@ async function quantidadeUsuarios() {
         xaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'Regioes do Brasil',
+                text: 'Regiões do Brasil',
                 font: {
                     color: getCSS('--cor-secundaria')
                 }
@@ -40,7 +40,7 @@ async function quantidadeUsuarios() {
         yaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'Bilhões de usuários ativos',
+                text: 'Número de localidades quilombolas',
                 font: {
                     color: getCSS('--cor-secundaria')
                 }
