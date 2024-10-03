f8ff826cf093da0585ae974007003d28d21501cc
Larissa C <104375004+Lariczak@users.noreply.github.com>
2024-10-03 11:53:47 -0300

    g

--- Gráficos/informacoesGlobais.js
+++ Gráficos/informacoesGlobais.js
@@ -1,18 +1,20 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json';
 
 async function visualizarInformacoesGlobais() {
-    const res = await fetch(url)
-    const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_conectadas/1e9)
-    const pessoasMundo = (dados.total_pessoas_mundo/1e9)
-    const horas = parseInt(dados.tempo_medio)
-    const minutos = Math.round((dados.tempo_medio - horas) *100)
-    const porcentagemConectada = ((pessoasConectadas/pessoasMundo)*100).toFixed(2)
-    const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${pessoasMundo} bilhões </span> de pessoas e que aproximadamente <span> ${pessoasConectadas} milhões </span> são de alguma comunidade quilombola e estão em cerca de <span> ${horas} </span> municípios?<br>Isso significa que <span>${porcentagemConectada} % </span> de pessoas são de alguma dessas comunidades. `
-    const container = document.getElementById('graficos-container')
-    container.appendChild(paragrafo)
-}
-visualizarInformacoesGlobais()
+    const res = await fetch(url);
+    const dados = await res.json();
+    
+    const pessoasConectadas = 1.32; // Alterado para 1,32 milhões
+    const pessoasMundo = 7.888; // Alterado para 7.888 bilhões
+    const municipios = 1672; // Alterado para 1.672 municípios
+    const porcentagemConectada = ((pessoasConectadas / (pessoasMundo * 1e3)) * 100).toFixed(2); // Cálculo baseado no novo valor
 
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container__texto');
+    
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} milhões</span> são de alguma comunidade quilombola e estão em cerca de <span>${municipios}</span> municípios?<br>Isso significa que <span>${porcentagemConectada} %</span> de pessoas são de alguma dessas comunidades.`;
+    
+    const container = document.getElementById('graficos-container');
+    container.appendChild(paragrafo);
+}
+visualizarInformacoesGlobais();
