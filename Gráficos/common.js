d1c3ed66f8152f00572714c50c84f0cea4a5ee0c
Larissa C <104375004+Lariczak@users.noreply.github.com>
2024-10-10 10:07:03 -0300

    x

--- Gráficos/common.js
+++ Gráficos/common.js
@@ -9,17 +9,3 @@ const tickConfig = {
 }
 
 export { getCSS, tickConfig }
-
-
-
-const getCSS = (variavel) => {
-    return getComputedStyle(document.body).getPropertyValue(variavel)
-}
-
-const tickConfig = {
-    color: getCSS('--cor-principal'),
-    size: 16,
-    family: getCSS('--fonte')
-}
-
-export { getCSS, tickConfig }
