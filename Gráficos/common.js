429d03b65e7d321ac4a227a171b8a0516d39385c
Larissa C <104375004+Lariczak@users.noreply.github.com>
2024-10-10 10:05:36 -0300

    g

--- Gráficos/common.js
+++ Gráficos/common.js
@@ -9,3 +9,17 @@ const tickConfig = {
 }
 
 export { getCSS, tickConfig }
+
+
+
+const getCSS = (variavel) => {
+    return getComputedStyle(document.body).getPropertyValue(variavel)
+}
+
+const tickConfig = {
+    color: getCSS('--cor-principal'),
+    size: 16,
+    family: getCSS('--fonte')
+}
+
+export { getCSS, tickConfig }
--- Gráficos/informacoesGlobais.js
+++ Gráficos/informacoesGlobais.js
@@ -18,3 +18,26 @@ async function visualizarInformacoesGlobais() {
     container.appendChild(paragrafo);
 }
 visualizarInformacoesGlobais();
+
+
+
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json';
+
+async function visualizarInformacoesGlobais() {
+    const res = await fetch(url);
+    const dados = await res.json();
+    
+    const pessoasConectadas = 1.32; // Alterado para 1,32 milhões
+    const pessoasMundo = 7.888; // Alterado para 7.888 bilhões
+    const municipios = 1672; // Alterado para 1.672 municípios
+    const porcentagemConectada = ((pessoasConectadas / (pessoasMundo * 1e3)) * 100).toFixed(2); // Cálculo baseado no novo valor
+
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container__texto');
+    
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} milhões</span> são de alguma comunidade quilombola e estão em cerca de <span>${municipios}</span> municípios?<br>Isso significa que <span>${porcentagemConectada} %</span> de pessoas são de alguma dessas comunidades.`;
+    
+    const container = document.getElementById('graficos-container');
+    container.appendChild(paragrafo);
+}
+visualizarInformacoesGlobais();
