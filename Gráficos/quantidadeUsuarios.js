23038b6c8bc688ef8cd5dd361c3e0bc6df212622
Larissa C <104375004+Lariczak@users.noreply.github.com>
2024-12-05 15:10:57 -0300

    w

--- Gráficos/quantidadeUsuarios.js
+++ Gráficos/quantidadeUsuarios.js
@@ -21,7 +21,7 @@ async function quantidadeUsuarios() {
         plot_bgcolor: getCSS('--cor-de-fundo'),
         paper_bgcolor: getCSS('--cor-de-fundo'),
         title: {
-            text: 'Comunidades Quilombolas no Brasil',
+            text: 'Veganismo no Brasil',
             font: {
                 color: getCSS('--cor-principal'),
                 family: getCSS('--fonte'),
